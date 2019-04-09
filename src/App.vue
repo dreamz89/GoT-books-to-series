69710fe9a9129555fb74733f2ed5ad6174572384
joctanys <joctanys@sph.com.sg>
2019-04-09 15:45:36 +0800

    added assets and some colors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
+      <intro></intro>
       <sankey></sankey>
       <barcharts></barcharts>
     </div>
@@ -8,12 +9,13 @@
 </template>
 
 <script>
+import Intro from './components/Intro.vue'
 import Sankey from './components/Sankey.vue'
 import Barcharts from './components/Barcharts.vue'
 
 export default {
   name: 'app',
-  components: { Sankey, Barcharts }
+  components: { Intro, Sankey, Barcharts }
 }
 </script>
 
@@ -22,6 +24,8 @@ body {
   margin: 0;
 }
 #app {
+  background-color: #101729;
+
   .container {
     max-width: 1200px;
     margin: 0 auto;
