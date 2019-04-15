d8433786da3c4626a69735227f300d10ab6ffac1
Jocelyn Tan <jocelyntan89@hotmail.com>
2019-04-16 00:17:11 +0800

    comment out season 5 data

--- src/components/Barcharts.vue
+++ src/components/Barcharts.vue
@@ -96,6 +96,26 @@ import S3E7 from '../data/GoT S3 E7.json'
 import S3E8 from '../data/GoT S3 E8.json'
 import S3E9 from '../data/GoT S3 E9.json'
 import S3E10 from '../data/GoT S3 E10.json'
+// import S5E1 from '../data/GoT S5 E1.json'
+// import S5E2 from '../data/GoT S5 E2.json'
+// import S5E3 from '../data/GoT S5 E3.json'
+// import S5E4 from '../data/GoT S5 E4.json'
+// import S5E5 from '../data/GoT S5 E5.json'
+// import S5E6 from '../data/GoT S5 E6.json'
+// import S5E7 from '../data/GoT S5 E7.json'
+// import S5E8 from '../data/GoT S5 E8.json'
+// import S5E9 from '../data/GoT S5 E9.json'
+// import S5E10 from '../data/GoT S5 E10.json'
+import S6E1 from '../data/GoT S6 E1.json'
+import S6E2 from '../data/GoT S6 E2.json'
+import S6E3 from '../data/GoT S6 E3.json'
+import S6E4 from '../data/GoT S6 E4.json'
+import S6E5 from '../data/GoT S6 E5.json'
+import S6E6 from '../data/GoT S6 E6.json'
+import S6E7 from '../data/GoT S6 E7.json'
+import S6E8 from '../data/GoT S6 E8.json'
+import S6E9 from '../data/GoT S6 E9.json'
+import S6E10 from '../data/GoT S6 E10.json'
 import S7E1 from '../data/GoT S7 E1.json'
 import S7E2 from '../data/GoT S7 E2.json'
 import S7E3 from '../data/GoT S7 E3.json'
@@ -112,6 +132,8 @@ export default {
         S1E1, S1E2, S1E3, S1E4, S1E5, S1E6, S1E7, S1E8, S1E9, S1E10,
         S2E1, S2E2, S2E3, S2E4, S2E5, S2E6, S2E7, S2E8, S2E9, S2E10,
         S3E1, S3E2, S3E3, S3E4, S3E5, S3E6, S3E7, S3E8, S3E9, S3E10,
+        // S5E1, S5E2, S5E3, S5E4, S5E5, S5E6, S5E7, S5E8, S5E9, S5E10,
+        S6E1, S6E2, S6E3, S6E4, S6E5, S6E6, S6E7, S6E8, S6E9, S6E10,
         S7E1, S7E2, S7E3, S7E4, S7E5, S7E6, S7E7
       },
       data: {},
