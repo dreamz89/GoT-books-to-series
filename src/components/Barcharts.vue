c1a47d99e61b62ca1239bb95c66011aba1e589cf
Jocelyn Tan <jocelyntan89@hotmail.com>
2019-04-25 12:37:00 +0800

    added data for the rest of season 4 and 5

--- src/components/Barcharts.vue
+++ src/components/Barcharts.vue
@@ -102,16 +102,21 @@ import S4E2 from '../data/GoT S4 E2.json'
 import S4E3 from '../data/GoT S4 E3.json'
 import S4E4 from '../data/GoT S4 E4.json'
 import S4E5 from '../data/GoT S4 E5.json'
+import S4E6 from '../data/GoT S4 E6.json'
+import S4E7 from '../data/GoT S4 E7.json'
+import S4E8 from '../data/GoT S4 E8.json'
+import S4E9 from '../data/GoT S4 E9.json'
+import S4E10 from '../data/GoT S4 E10.json'
 import S5E1 from '../data/GoT S5 E1.json'
 import S5E2 from '../data/GoT S5 E2.json'
 import S5E3 from '../data/GoT S5 E3.json'
-// import S5E4 from '../data/GoT S5 E4.json'
-// import S5E5 from '../data/GoT S5 E5.json'
-// import S5E6 from '../data/GoT S5 E6.json'
-// import S5E7 from '../data/GoT S5 E7.json'
-// import S5E8 from '../data/GoT S5 E8.json'
-// import S5E9 from '../data/GoT S5 E9.json'
-// import S5E10 from '../data/GoT S5 E10.json'
+import S5E4 from '../data/GoT S5 E4.json'
+import S5E5 from '../data/GoT S5 E5.json'
+import S5E6 from '../data/GoT S5 E6.json'
+import S5E7 from '../data/GoT S5 E7.json'
+import S5E8 from '../data/GoT S5 E8.json'
+import S5E9 from '../data/GoT S5 E9.json'
+import S5E10 from '../data/GoT S5 E10.json'
 import S6E1 from '../data/GoT S6 E1.json'
 import S6E2 from '../data/GoT S6 E2.json'
 import S6E3 from '../data/GoT S6 E3.json'
@@ -138,8 +143,8 @@ export default {
         S1E1, S1E2, S1E3, S1E4, S1E5, S1E6, S1E7, S1E8, S1E9, S1E10,
         S2E1, S2E2, S2E3, S2E4, S2E5, S2E6, S2E7, S2E8, S2E9, S2E10,
         S3E1, S3E2, S3E3, S3E4, S3E5, S3E6, S3E7, S3E8, S3E9, S3E10,
-        S4E1, S4E2, S4E3, S4E4, S4E5,
-        S5E1, S5E2, S5E3,
+        S4E1, S4E2, S4E3, S4E4, S4E5, S4E6, S4E7, S4E8, S4E9, S4E10,
+        S5E1, S5E2, S5E3, S5E4, S5E5, S5E6, S5E7, S5E8, S5E9, S5E10,
         S6E1, S6E2, S6E3, S6E4, S6E5, S6E6, S6E7, S6E8, S6E9, S6E10,
         S7E1, S7E2, S7E3, S7E4, S7E5, S7E6, S7E7
       },
